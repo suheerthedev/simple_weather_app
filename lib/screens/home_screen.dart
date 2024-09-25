c69988d4216be4d9622f951149c0cc29b940cf78
suheerthedev <khansuheer1@gmail.com>
2024-09-25 11:48:34 +0500

    Made a class to fetch api data, and constructed a basic structure for ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/models/weather_model.dart';
-import 'package:weather_app/services/weather_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
@@ -11,53 +9,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  //api key
-  final _weatherService = WeatherService('7b3b73136f46fc58b8e6d4e5c15dab50');
-  WeatherModel? _weather;
-
-  //fetch weather
-  _fetchWeather() async {
-    //get current city
-    String cityName = await _weatherService.getCurrentCity();
-
-    //get weather for city
-    try {
-      final weather = await _weatherService.getWeather(cityName);
-      setState(() {
-        _weather = weather;
-      });
-    }
-
-    //any errors
-    catch (e) {
-      print(e);
-    }
-  }
-
-  //weather animation
-
-  //init state
+  TextEditingController cityCont = TextEditingController();
   @override
   void initState() {
     super.initState();
-    //fetch weather on startup
-    _fetchWeather();
+    // fetch weather on startup
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            //city name
-            Text(_weather?.cityName ?? "loading city"),
-
-            //temperature
-            Text('${_weather?.temperature.round()}°C')
+            Text("City Name"),
+            Text("Weather Animation"),
+            Text("Temperature")
           ],
         ),
+      ),
+      floatingActionButton: Row(
+        children: [
+          TextField(
+            controller: cityCont,
+            decoration: const InputDecoration(hintText: "Enter City Name"),
+          ),
+          FloatingActionButton(onPressed: () {}),
+        ],
       ),
     );
   }
---
+++ lib/services/get_weather_api.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:weather_app/models/weather_model.dart';
+
+import 'package:http/http.dart' as http;
+
+class GetWeatherApi {
+  static const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  final String apiKey;
+
+  GetWeatherApi(this.apiKey);
+
+  Future<WeatherModel> getWeatherApi(String cityName) async {
+    var url = Uri.parse('$baseUrl?q=$cityName&appid=$apiKey&units=metric');
+    var reponse = await http.get(url);
+    var reponseBody = jsonDecode(reponse.body);
+
+    if (reponse.statusCode == 200) {
+      return WeatherModel.fromJson(reponseBody);
+    } else {
+      throw Exception("Failed to load weather :(");
+    }
+  }
+}
