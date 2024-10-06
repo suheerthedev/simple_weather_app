fb38a5662ea6476ab497ec64c66fbc633578e7c3
suheerthedev <khansuheer1@gmail.com>
2024-10-06 14:58:27 +0500

    Added Internet Access to app

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
+    // final screenHeight = MediaQuery.of(context).size.height;
     return MaterialApp(
       title: "Weather App",
       debugShowCheckedModeBanner: false,
